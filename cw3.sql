dcf4a38fb721d3ebd678723e60de6b39a90a2e0f
Aniela <131206657+4n1elk4@users.noreply.github.com>
2024-11-07 18:10:58 +0100

    Update cw3.sql

--- cw3.sql
+++ cw3.sql
@@ -1,5 +1,7 @@
 CREATE EXTENSION POSTGIS;
 
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2018_KAR_GERMANY\T2018_KAR_BUILDINGS.shp" b_2018 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2019_KAR_GERMANY\T2019_KAR_BUILDINGS.shp" b_2019 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
 --1
 CREATE TEMP TABLE changed_buildings as 
 SELECT b9.gid, 
@@ -13,6 +15,8 @@ WHERE NOT(ST_Equals(b8.geom, b9.geom))
 OR b8.height != b9.height
 OR b8.polygon_id is NULL;
 
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2018_KAR_GERMANY\T2018_KAR_POI_TABLE.shp" poi_2018 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2019_KAR_GERMANY\T2019_KAR_POI_TABLE.shp" poi_2019 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
 --2
 WITH new_poi as (
 	SELECT p9.poi_id,
@@ -29,8 +33,9 @@ FROM changed_buildings as cb
 JOIN new_poi as np
 ON ST_Intersects(np.geom, ST_Buffer(ST_SetSRID(cb.geom, 3068), 500))
 GROUP BY np.type
-ORDER BY counts desc
-	
+ORDER BY counts desc;
+
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2019_KAR_GERMANY\T2019_KAR_STREETS.shp" streets_2019 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
 --3
 CREATE TABLE streets_reprojected as
 SELECT gid,
@@ -59,6 +64,7 @@ VALUES ('POINT(8.36093 49.03174)'),
 UPDATE input_points
 SET geom = ST_SetSRID(geom,3068);
 
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2019_KAR_GERMANY\T2019_KAR_STREET_NODE.shp" street_node_2019 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
 --6
 UPDATE street_node_2019
 SET geom = ST_SetSRID(geom,3068);
@@ -77,6 +83,7 @@ FROM street_node_2019 as sn
 CROSS JOIN line
 WHERE ST_Contains(ST_Buffer(line.geom, 200),sn.geom);
 
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2019_KAR_GERMANY\T2019_KAR_LAND_USE_A.shp" land_use_a_2019 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
 --7
 UPDATE land_use_a_2019
 SET geom = ST_SetSRID(geom,3068);
@@ -89,8 +96,10 @@ FROM land_use_a_2019 as lu
 JOIN poi_2019 as poi
 ON ST_DWithin(lu.geom, poi.geom, 300)
 WHERE poi.type='Sporting Goods Store'
-AND lu.type ilike '%park%'
+AND lu.type ilike '%park%';
 
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2019_KAR_GERMANY\T2019_KAR_RAILWAYS.shp" railways_2019 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
+-- shp2pgsql "C:\[...]\Karlsruhe_Germany_Shapefile\T2019_KAR_GERMANY\T2019_KAR_WATER_LINES.shp" water_lines_2019 | psql -U postgres -h localhost -p 5432 -d bdp_cw3
 --8
 CREATE TABLE T2019_KAR_BRIDGES as 
 	SELECT ST_Intersection(wl.geom, r.geom) as geom
